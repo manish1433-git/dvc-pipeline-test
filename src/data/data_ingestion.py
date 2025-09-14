cdd29ec1bd0cb035d0eec86948d8368fd13899de
Manish Priyadarshan <manishpriyadarshan48@gmail.com>
2025-09-15 00:44:11 +0530

    happiness added as data

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv('https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv')
 
 df = df.drop('tweet_id', axis=1)
-final_df = df[df['sentiment'].isin(['neutral','sadness'])].copy()
+final_df = df[df['sentiment'].isin(['happiness','sadness'])].copy()
 final_df['sentiment'] = final_df['sentiment'].replace({'happiness':0,'sadness':1})
 
 train_data,test_data=train_test_split(final_df,test_size=0.2,random_state=42)
